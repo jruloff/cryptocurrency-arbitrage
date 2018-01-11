0d68454d818075e57391732728bd497aec5e4c17
Justin Ruloff <jruloff@umich.edu>
2018-01-10 21:46:59 -0600

    remove changed API call
    - btc38 returning invalid json, most likely due to marketplace api change

--- settings.js
+++ settings.js
@@ -90,33 +90,6 @@ let markets = [
     },
 
     {
-        marketName: 'btc38',
-        URL: 'http://api.btc38.com/v1/ticker.php?c=all&mk_type=cny',
-        toBTCURL: false,
-        pairURL : '',
-        last: function (data, coin_prices, toBTCURL) { //Where to find the last price of coin in JSON data
-            return new Promise(function (res, rej) {
-                let priceOfBTC = data.btc.ticker.last;
-                try {
-                    for (let key in data) {
-                        let coinName = key.toUpperCase();
-                        let price = data[key]['ticker'].last;
-                        if (!coin_prices[coinName]) coin_prices[coinName] = {};
-
-                        coin_prices[coinName]["btc38"] = data[key]['ticker'].last / priceOfBTC;
-                    }
-                    res(coin_prices);
-                }
-
-                catch (err) {
-                    console.log(err);
-                    rej(err)
-                }
-            })
-        }
-    },
-
-    {
         marketName: 'jubi',
         URL: 'https://www.jubi.com/api/v1/allticker/', //URL To Fetch API From.
         toBTCURL: false, //URL, if needed for an external bitcoin price api.
